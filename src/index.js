9d106ddcfc2071cada07436db256451e1b11105c
Zidane Innis <zidaneinnis@gmail.com>
2023-01-24 17:04:24 -0500

    Babel use and Demo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello from webpack dev server");
+const foo = (name) => {
+  console.log(`Hello ${name}`);
+};
+
+foo("Zidane");
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,21 @@ module.exports = {
   devServer: {
     static: "./dist",
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       title: "Development",
